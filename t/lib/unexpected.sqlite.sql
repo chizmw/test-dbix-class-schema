5369461d428cb202ffc1f9d4a9710efb9765d7e4
Chisel <chisel@chizography.net>
2013-03-04 21:11:35 +0000

    Remove BEGIN/COMMIT when creating spanish_inquisition
    This prevents:
    
      DBIx::Class::Storage::DBI::dbh(): Internal transaction state of handle
      DBI::db=HASH(0x331a2d0) (apparently in a transaction) does not seem to match
      its AutoCommit attribute setting of TRUE - this is an indication of a
      potentially serious bug in your transaction handling logic at
      t/lib/DBHelper.pm line 73
    
    Caused by doing a BEGIN/COMMIT in the 'wrong place'.
    
    I'm not sure why we didn't see this before.

--- t/lib/unexpected.sqlite.sql
+++ t/lib/unexpected.sqlite.sql
@@ -1,8 +1,6 @@
 -- This creates a basis for the schema for testing the 'unexpected columns in
 -- DB' functioanlity added by jason
-BEGIN TRANSACTION;
-    CREATE TABLE spanish_inquisition (
-        id integer          PRIMARY KEY AUTOINCREMENT,
-        name varchar(255)
-    );
-COMMIT;
+CREATE TABLE spanish_inquisition (
+    id integer          PRIMARY KEY AUTOINCREMENT,
+    name varchar(255)
+);
